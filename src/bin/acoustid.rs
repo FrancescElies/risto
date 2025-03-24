8e67f85396d122a5f6d564846a1d26ce52e4e700
Francesc Elies <elies@posteo.net>
2025-03-24 22:30:22 +0100

    feat: accept files from stdin

--- src/bin/acoustid.rs
+++ src/bin/acoustid.rs
@@ -4,7 +4,10 @@ use risto::{
     acoustid::{self, rename_file_as_artist_dash_title},
     mp3_files, Song,
 };
-use std::path::{Path, PathBuf};
+use std::{
+    io,
+    path::{Path, PathBuf},
+};
 
 use clap::Parser;
 
@@ -14,23 +17,30 @@ use clap::Parser;
 #[derive(Parser)]
 struct Cli {
     /// file or folder to calculate acoustid
-    path: PathBuf,
+    path: Option<PathBuf>,
 }
 
 fn main() -> Result<()> {
     let args = Cli::parse();
     let path = args.path;
-    let path_str = path.to_string_lossy();
+    let files = match path {
+        Some(path) => {
+            let path_str = path.to_string_lossy();
 
-    let path = shellexpand::full(&path_str)
-        .with_context(|| format!("couldn't expand {}", &path.display()))?;
-    let path = Path::new(path.as_ref());
+            let path = shellexpand::full(&path_str)
+                .with_context(|| format!("couldn't expand {}", &path.display()))?;
+            let path = Path::new(path.as_ref());
+
+            mp3_files(path)
+        }
+        None => read_files_from_stdin(),
+    };
 
     let _ = std::env::var("ACOUSTID_API_KEY").with_context(|| {
         "reading env var ACOUSTID_API_KEY, register app at https://acoustid.org/my-applications or use the same client as in examples at https://acoustid.org/webservice"
     })?;
 
-    let (newfiles, errors): (Vec<_>, Vec<_>) = mp3_files(path)
+    let (newfiles, errors): (Vec<_>, Vec<_>) = files
         .par_iter()
         .map(|song| lookup_write_id3_and_rename_file(song))
         // .collect::<Vec<Result<_>>>();
@@ -49,6 +59,16 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn read_files_from_stdin() -> Vec<PathBuf> {
+    let mut lines = vec![];
+    for line in io::stdin().lines() {
+        if let Ok(x) = line {
+            lines.push(PathBuf::from(x));
+        }
+    }
+    lines
 }
 
 fn lookup_write_id3_and_rename_file(file: &PathBuf) -> Result<PathBuf> {
