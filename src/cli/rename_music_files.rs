6cdef80f17f62b2088b936711793d711a6a73cfb
Francesc Elies <elies@posteo.net>
2025-03-25 14:40:31 +0100

    .

--- src/cli/rename_music_files.rs
+++ src/cli/rename_music_files.rs
@@ -7,13 +7,14 @@ use risto::{
 use std::path::PathBuf;
 
 fn lookup_write_id3_and_rename_file(file: &PathBuf) -> Result<PathBuf> {
-    eprintln!("\n# File `{}`", file.display());
-    let song = Song::new(file).with_context(|| ("❌ open song failed"))?;
-    let song_data =
-        lookup_by_fingerprint(song).with_context(|| "❌ fingerprint lookup failed: {err}")?;
-    write_song_data(file, &song_data).with_context(|| "❌ write id3 failed")?;
-    let newfile =
-        rename_file_as_artist_dash_title(file).with_context(|| "❌ rename file failed")?;
+    let filename = file.display();
+    eprintln!("\n# File `{}`", filename);
+    let song = Song::new(file).with_context(|| format!("❌ open song failed {filename}"))?;
+    let song_data = lookup_by_fingerprint(song)
+        .with_context(|| format!("❌ fingerprint lookup failed {filename}"))?;
+    write_song_data(file, &song_data).with_context(|| format!("❌ write id3 failed {filename}"))?;
+    let newfile = rename_file_as_artist_dash_title(file)
+        .with_context(|| format!("❌ rename file failed {filename}"))?;
     Ok(newfile)
 }
 
